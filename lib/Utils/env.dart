80c9403fa86475a9f882531490890a1980898520
rhillx <rhillx.code@gmail.com>
2022-02-26 16:57:42 -0500

    fixed env

--- lib/Utils/env.dart
+++ lib/Utils/env.dart
@@ -2,10 +2,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Env {
   static init() async {
-    await dotenv.load(fileName: 'assets/env/.env_base');
+    await dotenv.load(fileName: '.env');
     assert(dotenv.env.containsKey("ENV"));
     final envName = dotenv.env['ENV'];
-    await dotenv.load(fileName: 'assets/env/.env_$envName');
+    await dotenv.load(fileName: 'assets/env/.env.$envName');
 
     assert(dotenv.env.containsKey("ENVIRONMENT_NAME"));
     assert(dotenv.env.containsKey("API_BASE_URL"));
