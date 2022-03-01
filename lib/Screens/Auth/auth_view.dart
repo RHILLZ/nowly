589a9b2d94e7d35c77c4b19a089663d1f9af128e
kimvillanueva97 <kimvillanueva.it@gmail.com>
2022-03-01 14:19:53 +0800

    NWLY-1: Fix Signin bug

--- lib/Screens/Auth/auth_view.dart
+++ lib/Screens/Auth/auth_view.dart
@@ -85,7 +85,7 @@ class AuthView extends GetView<AuthController> {
                 child: AuthButton(
                   color: Get.isDarkMode ? Colors.white : kSecondaryColor,
                   onPressed: () {
-                    controller.agreedToTerms
+                    onboardSelection != "newAccount" || controller.agreedToTerms
                         ? AppleAuth().signInWithApple()
                         : controller.termsWarningSnackbar();
                   },
