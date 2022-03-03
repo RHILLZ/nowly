80d4f3993026d1861ede73bd2ace8bf38794bbbb
kimvillanueva97 <kimvillanueva.it@gmail.com>
2022-03-04 07:35:46 +0800

    NWLY-2: User Info check with Linter standard update

--- lib/Services/Google/google_auth.dart
+++ lib/Services/Google/google_auth.dart
@@ -1,14 +1,22 @@
+// ignore_for_file: omit_local_variable_types
+
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:nowly/Controllers/controller_exporter.dart';
 import 'package:nowly/Screens/OnBoarding/user_registration_view.dart';
-import 'package:nowly/Utils/logger.dart';
+import 'package:nowly/Services/Firebase/firebase_futures.dart';
 import 'package:nowly/root.dart';
 
+/// Google Signin Service
 class GoogleAuth {
+  /// Get User's Authentication instance
   AuthController authController = Get.put(AuthController());
-  void signInWithGoogle() async {
+
+  /// logs in the user by Google Signin service
+  Future<void> signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
@@ -19,12 +27,24 @@ class GoogleAuth {
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
-      UserCredential authUser =
+
+      final UserCredential authUser =
           await authController.auth.signInWithCredential(credential);
 
-      authUser.additionalUserInfo!.isNewUser
-          ? Get.off(UserRegistrationView())
-          : Get.off(const Root());
+      if(authUser.user != null) {
+        final id = (authUser.user?.uid)??'';
+
+        if(id.isNotEmpty) {
+          final _user = await FirebaseFutures().getUserInFirestoreInstance(id);
+          if(!_user.exists){
+            unawaited(Get.off(UserRegistrationView()));
+          } else {
+            unawaited(Get.off(const Root()));
+          }
+        }
+      } else {
+        unawaited(Get.off(UserRegistrationView()));
+      }
     } catch (e) {
       Get.snackbar('Error signing in with goole account', e.toString());
     }
