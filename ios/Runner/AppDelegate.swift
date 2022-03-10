1d90780679913b14e14c170464e5fab555a26e25
rhillx <rhillx.code@gmail.com>
2022-03-10 18:23:58 -0500

    fixed GOOGLE_MAP_API in android and ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCE4G48mdZMnKgVCTpqxVDORIEzq6Dl1lA")
+    GMSServices.provideAPIKey("AIzaSyChmrec5H8gfgkGHswbGjViapaRFb0R2bM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
